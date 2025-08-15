1e3ffdf29276000a6df0818b31f54ff8a899c7a9
su-myn <134207439+su-myn@users.noreply.github.com>
2025-08-16 02:47:17 +0800

    create baseline migration

---
+++ migrations/versions/cdec6e957113_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: cdec6e957113
+Revises: 
+Create Date: 2025-08-16 02:44:52.075851
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cdec6e957113'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
